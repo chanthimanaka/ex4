9df7980e0b91d4ac5ed1c9334417468a2bf366ad
Nidchara Chanthima <60143403@g.cmru.ac.th>
2019-07-14 10:32:22 +0700

    12

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index");
 });
 
+router.get("/create", function(req, res, next) {
+  res.render("create");
+});
+
+router.get("/login", function(req, res, next) {
+  res.render("login");
+});
 module.exports = router;
